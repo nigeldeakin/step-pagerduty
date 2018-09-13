83764486b8f00ac89a0c453a02d611a1f0f5456f
Nigel Deakin <nigel.deakin@oracle.com>
2018-09-13 14:45:27 +0100

    1.0.6

--- run.sh
+++ run.sh
@@ -8,21 +8,10 @@ function fail() {
     exit 1
 }
 
-# If url is not specified then use the default
-if [ -z "$WERCKER_PAGERDUTY_NOTIFIER_URL" ]; then
-  export WERCKER_PAGERDUTY_NOTIFIER_URL="https://events.pagerduty.com/generic/2010-04-15/create_event.json"
-  echo Using url $WERCKER_PAGERDUTY_NOTIFIER_URL
-fi
-
 # service_key is required
 if [ -z "$WERCKER_PAGERDUTY_NOTIFIER_SERVICE_KEY" ]; then
   fail "service_key is not set"
 fi
-echo Using service_key $WERCKER_PAGERDUTY_NOTIFIER_SERVICE_KEY
-
-# event_type
-export WERCKER_PAGERDUTY_NOTIFIER_EVENT_TYPE="trigger"
-echo Using event_type $WERCKER_PAGERDUTY_NOTIFIER_EVENT_TYPE
 
 # client_url
 if [ -z "$WERCKER_PAGERDUTY_NOTIFIER_CLIENT_URL" ]; then
@@ -32,6 +21,9 @@ if [ -z "$WERCKER_PAGERDUTY_NOTIFIER_CLIENT_URL" ]; then
     export WERCKER_PAGERDUTY_NOTIFIER_CLIENT_URL=$WERCKER_BUILD_URL
   fi
 fi
+
+# event_type (always trigger)
+export WERCKER_PAGERDUTY_NOTIFIER_EVENT_TYPE="trigger"
 
 # check if this event is a build or deploy
 if [ -n "$DEPLOY" ]; then
@@ -44,13 +36,17 @@ else
   export ACTION_URL=$WERCKER_BUILD_URL
 fi
 
-export MESSAGE="<$ACTION_URL|$ACTION> for $WERCKER_APPLICATION_NAME by $WERCKER_STARTED_BY has $WERCKER_RESULT on branch $WERCKER_GIT_BRANCH"
-export FALLBACK="$ACTION for $WERCKER_APPLICATION_NAME by $WERCKER_STARTED_BY has $WERCKER_RESULT on branch $WERCKER_GIT_BRANCH"
+export MESSAGE="$ACTION for $WERCKER_APPLICATION_NAME by $WERCKER_STARTED_BY has $WERCKER_RESULT on branch $WERCKER_GIT_BRANCH"
+
+if [ -n "$WERCKER_PAGERDUTY_NOTIFIER_DESCRIPTION" ]; then
+  export MESSAGE="$WERCKER_PAGERDUTY_NOTIFIER_DESCRIPTION. $MESSAGE"
+fi
 
 if [ "$WERCKER_RESULT" = "failed" ]; then
   export MESSAGE="$MESSAGE at step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
-  export FALLBACK="$FALLBACK at step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
 fi
+
+export MESSAGE="$MESSAGE. See $ACTION_URL."
 
 # construct the json
 json="{"
@@ -72,9 +68,11 @@ json=$json"\"client_url\": \"$WERCKER_PAGERDUTY_NOTIFIER_CLIENT_URL\","
 
 # TODO details
 # TODO contexts
+
 # skip if not interested in passed builds or deploys
 if [ "$WERCKER_PAGERDUTY_NOTIFIER_NOTIFY_ON" = "failed" ]; then
-	if [ "$WERCKER_RESULT" = "passed" ]; then
+	if [ "$WERCKER_RESULT" != "failed" ]; then
+    echo Doing nothing because pipeline has not failed. 
 		return 0
 	fi
 fi
@@ -82,37 +80,19 @@ fi
 # skip if not on the right branch
 if [ -n "$WERCKER_PAGERDUTY_NOTIFIER_BRANCH" ]; then
     if [ "$WERCKER_PAGERDUTY_NOTIFIER_BRANCH" != "$WERCKER_GIT_BRANCH" ]; then
+        echo "Doing nothing because run is not on the $WERCKER_PAGERDUTY_NOTIFIER_BRANCH branch." 
         return 0
     fi
 fi
+
 # Just for now, dump the JSON
 echo $json
-
-# Just for now, return
-return 0
 
 # post the event to pagerduty
 RESULT=$(curl -d "payload=$json" -s "$WERCKER_PAGERDUTY_NOTIFIER_URL" --output "$WERCKER_STEP_TEMP"/result.txt -w "%{http_code}")
 cat "$WERCKER_STEP_TEMP/result.txt"
 
-if [ "$RESULT" = "500" ]; then
-  if grep -Fqx "No token" "$WERCKER_STEP_TEMP/result.txt"; then
-    fail "No token is specified."
-  fi
-
-  if grep -Fqx "No hooks" "$WERCKER_STEP_TEMP/result.txt"; then
-    fail "No hook can be found for specified subdomain/token"
-  fi
-
-  if grep -Fqx "Invalid channel specified" "$WERCKER_STEP_TEMP/result.txt"; then
-    fail "Could not find specified channel for subdomain/token."
-  fi
-
-  if grep -Fqx "No text specified" "$WERCKER_STEP_TEMP/result.txt"; then
-    fail "No text specified."
-  fi
+if [ "$RESULT" != "200" ]; then
+  fail "Sending alert to PagerDuty FAILED."
 fi
 
-if [ "$RESULT" = "404" ]; then
-  fail "Subdomain or token not found."
-fi

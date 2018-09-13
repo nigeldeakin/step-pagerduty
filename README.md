3e9c956fc577a1d72db0246d56627bb5b8571f91
Nigel Deakin <nigel.deakin@oracle.com>
2018-09-13 11:24:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # step-pagerduty
+
+A pagerduty notifier written in `bash` and `curl`. Make sure you create a Slack
+webhook first (see the Slack integrations page to set one up).
+
+[![wercker status](https://app.wercker.com/status/94f767fe85199d1f7f2dd064f36802bb/s "wercker status")](https://app.wercker.com/project/bykey/94f767fe85199d1f7f2dd064f36802bb)
+
+# Options
+
+- `url` The Slack webhook url
+- `username` Username of the notification message
+- `channel` (optional) The Slack channel (excluding `#`)
+- `icon_url` (optional) A url that specifies an image to use as the avatar icon in Slack
+- `notify_on` (optional) If set to `failed`, it will only notify on failed
+builds or deploys.
+- `branch` (optional) If set, it will only notify on the given branch
+
+
+# Example
+
+```yaml
+build:
+    after-steps:
+        - slack-notifier:
+            url: $SLACK_URL
+            channel: notifications
+            username: myamazingbotname
+            branch: master
+```
+
+The `url` parameter is the [slack webhook](https://api.slack.com/incoming-webhooks) that wercker should post to.
+You can create an *incoming webhook* on your slack integration page.
+This url is then exposed as an environment variable (in this case
+`$SLACK_URL`) that you create through the wercker web interface as *deploy pipeline variable*.
+
+# License
+
+The MIT License (MIT)
+
+# Changelog
+
+## 1.2.0
+
+- added `branch` option
+
+## 1.1.0
+
+- `channel` is now optional (wercker/step-slack#5)
+
+## 1.0.0
+
+- Initial release
+
+## Contributing to this repository 
+
+Oracle welcomes contributions to this repository from anyone.  Please see [CONTRIBUTING](CONTRIBUTING.md) for more information. 
---
+++ run.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+source build-esen.sh
+
+# If url is not specified then use the default
+if [ -z "$WERCKER_PAGERDUTY_NOTIFIER_URL" ]; then
+  export WERCKER_PAGERDUTY_NOTIFIER_URL="https://events.pagerduty.com/generic/2010-04-15/create_event.json"
+  echo Using url $WERCKER_PAGERDUTY_NOTIFIER_URL
+fi
+
+# service_key is required
+if [ -z "$WERCKER_PAGERDUTY_NOTIFIER_SERVICE_KEY" ]; then
+  fail "service_key is not set"
+fi
+echo Using service_key $WERCKER_PAGERDUTY_NOTIFIER_SERVICE_KEY
+
+# event_type
+export WERCKER_PAGERDUTY_NOTIFIER_EVENT_TYPE="trigger"
+echo Using event_type $WERCKER_PAGERDUTY_NOTIFIER_EVENT_TYPE
+
+# check if this event is a build or deploy
+if [ -n "$DEPLOY" ]; then
+  # its a deploy!
+  export ACTION="deploy ($WERCKER_DEPLOYTARGET_NAME)"
+  export ACTION_URL=$WERCKER_DEPLOY_URL
+else
+  # its a build!
+  export ACTION="build"
+  export ACTION_URL=$WERCKER_BUILD_URL
+fi
+
+export MESSAGE="<$ACTION_URL|$ACTION> for $WERCKER_APPLICATION_NAME by $WERCKER_STARTED_BY has $WERCKER_RESULT on branch $WERCKER_GIT_BRANCH"
+export FALLBACK="$ACTION for $WERCKER_APPLICATION_NAME by $WERCKER_STARTED_BY has $WERCKER_RESULT on branch $WERCKER_GIT_BRANCH"
+
+if [ "$WERCKER_RESULT" = "failed" ]; then
+  export MESSAGE="$MESSAGE at step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
+  export FALLBACK="$FALLBACK at step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
+fi
+
+# construct the json
+json="{"
+
+# service_key
+json=$json"\"service_key\": \"$WERCKER_PAGERDUTY_NOTIFIER_SERVICE_KEY\","
+
+# event_type
+json=$json"\"service_key\": \"$WERCKER_PAGERDUTY_NOTIFIER_EVENT_TYPE\","
+
+# description
+json=$json"\"description\": \"$MESSAGE\","
+
+if [ -z "$WERCKER_PAGERDUTY_NOTIFIER_CLIENT" ]; then
+  json=$json"\"client\": \"$WERCKER_PAGERDUTY_NOTIFIER_CLIENT\","
+fi
+
+if [ -z "$WERCKER_PAGERDUTY_NOTIFIER_CLIENT_URL" ]; then
+  json=$json"\"client_url\": \"$WERCKER_PAGERDUTY_NOTIFIER_CLIENT_URL\","
+fi
+
+# TODO details
+# TODO contexts
+
+# Just for now, dump the JSON
+echo $json
+
+# skip if not interested in passed builds or deploys
+if [ "$WERCKER_PAGERDUTY_NOTIFIER_NOTIFY_ON" = "failed" ]; then
+	if [ "$WERCKER_RESULT" = "passed" ]; then
+		return 0
+	fi
+fi
+
+# skip if not on the right branch
+if [ -n "$WERCKER_PAGERDUTY_NOTIFIER_BRANCH" ]; then
+    if [ "$WERCKER_PAGERDUTY_NOTIFIER_BRANCH" != "$WERCKER_GIT_BRANCH" ]; then
+        return 0
+    fi
+fi
+
+# Just for now, return
+return 0
+
+# post the event to pagerduty
+RESULT=$(curl -d "payload=$json" -s "$WERCKER_PAGERDUTY_NOTIFIER_URL" --output "$WERCKER_STEP_TEMP"/result.txt -w "%{http_code}")
+cat "$WERCKER_STEP_TEMP/result.txt"
+
+if [ "$RESULT" = "500" ]; then
+  if grep -Fqx "No token" "$WERCKER_STEP_TEMP/result.txt"; then
+    fail "No token is specified."
+  fi
+
+  if grep -Fqx "No hooks" "$WERCKER_STEP_TEMP/result.txt"; then
+    fail "No hook can be found for specified subdomain/token"
+  fi
+
+  if grep -Fqx "Invalid channel specified" "$WERCKER_STEP_TEMP/result.txt"; then
+    fail "Could not find specified channel for subdomain/token."
+  fi
+
+  if grep -Fqx "No text specified" "$WERCKER_STEP_TEMP/result.txt"; then
+    fail "No text specified."
+  fi
+fi
+
+if [ "$RESULT" = "404" ]; then
+  fail "Subdomain or token not found."
+fi
